9199105314d8bd4a2b713768b367af488ae4ae36
Sadhna singh <sadhna.singh.work@gmail.com>
2022-12-03 17:30:58 +1300

    made some changes in code

--- client/components/DogList.jsx
+++ client/components/DogList.jsx
@@ -42,11 +42,6 @@ function DogList() {
           </>
         )
       })}
-
-      <img
-        src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg"
-        alt="doggy"
-      />
     </>
   )
 }
