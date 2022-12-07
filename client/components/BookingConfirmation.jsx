650cc5bb7e43552bdcfa36e6c90d0611b7bf47ba
elvintern <elvintern@gmail.com>
2022-12-07 14:49:23 +1300

    Add styling for a walker form and modify general class style

--- client/components/BookingConfirmation.jsx
+++ client/components/BookingConfirmation.jsx
@@ -6,13 +6,11 @@ function BookingConfirmation() {
   return (
     <>
       <NavLink />
-      <div className="dog-description">
-        <div className="card card-owner">
-          <p className="about-paragraph">
-            Thanks for your interest in walking this doggo! Your request has
-            been sent to the dog owner and we will be in touch soon. &#128062;
-          </p>
-        </div>
+      <div className="card-confirm">
+        <p className="about-paragraph confirm-paragraph">
+          Thanks for your interest in walking this doggo! Your request has been
+          sent to the dog owner and we will be in touch soon. &#128062;
+        </p>
       </div>
     </>
   )
