329a1e0dae27935dddf6a3a9c1688b59038870bc
ahmad anwar <eng.ahmed.anwar@gmail.com>
2023-02-10 19:36:49 +1300

    configure production

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -29,28 +29,10 @@ module.exports = {
     },
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
-
   production: {
-    client: 'sqlite3',
+    client: 'pg',
     useNullAsDefault: true,
-    connection: {
-      filename: path.join(__dirname, 'dev.sqlite3'),
-    },
+    connection: process.env.PG_CONNECTION_STRING,
     migrations: {
       directory: path.join(__dirname, 'migrations'),
     },
