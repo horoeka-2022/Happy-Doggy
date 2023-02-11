814378270d9db748a0bded2e39f2d832f73a5e6f
elvintern <elvintern@gmail.com>
2023-02-11 23:40:00 +1300

    Revert "add production connection string"
    This reverts commit 6ea658c68629e111a6c497d6bff386b49426f384.

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,7 +1,4 @@
 const path = require('path')
-const dotenv = require('dotenv')
-
-dotenv.config({ path: path.join(__dirname, '..', '..', '.env') })
 
 module.exports = {
   development: {
@@ -33,18 +30,14 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      connectionString: process.env.PG_CONNECTION_STRING,
-      ssl: { rejectUnauthorized: false },
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
+    client: 'pg',
     useNullAsDefault: true,
+    connection: process.env.PG_CONNECTION_STRING,
+    migrations: {
+      directory: path.join(__dirname, 'migrations'),
+    },
+    seeds: {
+      directory: path.join(__dirname, 'seeds'),
+    },
   },
 }
